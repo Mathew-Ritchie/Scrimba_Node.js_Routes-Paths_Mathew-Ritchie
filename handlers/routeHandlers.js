7ad96a4a796a38fc665f034e7bc6f77e92fbd2cf
Mathew Ritchie <ritchmat89@gmail.com>
2025-10-15 16:13:55 +0200

    event stream functionality so that browser info updates automotically.

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -4,6 +4,7 @@ import { parseJSONBody } from "../public/utils/parseJSONBody.js";
 import { addNewSighting } from "../public/utils/addNewSighting.js";
 import { sanitizeInput } from "../public/utils/sanitizeInput.js";
 import { sightingEvents } from "../events/sightingEvents.js";
+import { stories } from "../data/stories.js";
 
 export async function handleGet(res) {
   const data = await getData();
@@ -25,4 +26,28 @@ export async function handlePost(req, res) {
       "application/json"
     );
   }
+}
+
+export async function handleNews(req, res) {
+  res.statusCode = 200;
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  /*
+Challenge 1:
+  1. Set Content-Type, Cache-Control, and Connection headers
+*/
+
+  setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * stories.length);
+
+    res.write(
+      `data: ${JSON.stringify({
+        event: "news-update",
+        story: stories[randomIndex],
+      })}\n\n`
+    );
+  }, 3000);
 }
